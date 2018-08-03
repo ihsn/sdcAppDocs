b97163f4380704c39e2b07d5ac7ad2c3dbcf0a5c
mwelch750 <mwelch750@gmail.com>
2018-08-03 10:39:25 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -74,15 +74,13 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'agogo'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {
-textalign = justify
-documentwidth = 50em}
+# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
